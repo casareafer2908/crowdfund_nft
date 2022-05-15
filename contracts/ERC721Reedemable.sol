3da9e7bbf36a3da09c1371a4937e43b8c2231f0f
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2022-05-15 18:09:28 +0800

    Update ERC721Reedemable.sol
    remove irrelevant comments

--- contracts/ERC721Reedemable.sol
+++ contracts/ERC721Reedemable.sol
@@ -6,9 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
 abstract contract ERC721Reedemable is Context, Ownable {
-    //states:
-    //0 -> OFF - People can't redeem
-    //1 -> ON - People can redeem
     enum Redeem_State {
         OFF,
         ON
@@ -19,10 +16,7 @@ abstract contract ERC721Reedemable is Context, Ownable {
         redeem_state = Redeem_State.OFF;
     }
 
-    //maps
     mapping(uint256 => uint256) public tokenIdToRemainingRedeems;
-
-    //events
     event SetTokenLimit(uint256 tokenId, uint256 redeemsLimit);
     event SetAllTokensLimit(uint256 redeemsLimit);
     event redeemGoods(uint256 redeemAmount, uint256 redeemLeft);
@@ -63,7 +57,6 @@ abstract contract ERC721Reedemable is Context, Ownable {
         return active;
     }
 
-    //internal since it can only be opened when the goal is met
     function _setRedeemState(bool state) internal onlyOwner {
         redeem_state = (state == true ? Redeem_State.ON : Redeem_State.OFF);
     }
